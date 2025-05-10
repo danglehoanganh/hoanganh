f78ac452bf120ba1aa57bd978be06ca5d3d7ea38
danglehoanganh <134114602+danglehoanganh@users.noreply.github.com>
2025-05-10 14:25:34 +0700

    Delete hoanganh-main directory

--- hoanganh-main/QuanLySinhVien/mongodata.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-# db_connection.py
-from pymongo import MongoClient
-
-def get_database():
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["university"]
-    return db
